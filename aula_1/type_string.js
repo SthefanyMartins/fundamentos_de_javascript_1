cc483e3bdee772582b4e9641ebc72806509f4635
Sthefany Martins <sthefanymartins1505@gmail.com>
2022-06-15 10:59:31 -0300

    Curso de Fundamentos de JavaScript 1

---
+++ aula_1/type_string.js
@@ -0,0 +1,20 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'o Leo disse "Oi!"';
+console.log(citacao);
+
+const citacao2 = "o Leo disse 'Oi!'";
+console.log(citacao2);
+
+//concatenacao (+)
+
+const citacao3 = "Meu nome é ";
+const meuNome = "Sthefany";
+console.log(citacao3 + meuNome);
+
+
+//template string ou template literal
+
---
+++ aula_2/conversoes.js
@@ -0,0 +1,20 @@
+// tipo de dado 
+// booleanos 
+
+// conversão implícita
+const numero = 456;
+//const numeroString = "456";
+//const numeroString = Number("456");
+const numeroString = Number("456a");
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+//Number()
+//String()
+//console.log(numero + Number(numeroString));
+console.log(numero + numeroString);
+
+// conversão explícita
+
+console.warn(new Error('oi'));
